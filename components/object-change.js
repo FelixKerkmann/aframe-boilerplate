1a94254a27f16a931f0193bafdc7db8587a6d80f
FelixKerkmann <felix.kerkmann@students.fhnw.ch>
2022-03-23 09:27:14 +0100

    added object rotation

---
+++ components/object-change.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent('change-object', {
+    schema: {
+        visibility: { default: '' }
+    },
+    init: function(){
+        var data = this.data;
+        this.el.addEventListener('click', function(){
+            let o1 = document.querySelector('#pool');
+            let o2 = document.querySelector('#motor');
+            let o3 = document.querySelector('#gym-run-jog');
+            if (o1.getAttribute('visible') === true) {
+                o1.setAttribute('visible', 'false');
+                o2.setAttribute('visible', 'true');
+            } else if (o2.getAttribute('visible') === true){
+                o2.setAttribute('visible', 'false');
+                o3.setAttribute('visible', 'true');
+            } else {
+                o3.setAttribute('visible', 'false');
+                o1.setAttribute('visible', 'true');
+            }
+        })
+    }
+});
