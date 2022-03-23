896df7a716e31fb7c7847b0605460b7babede98d
FelixKerkmann <felix.kerkmann@students.fhnw.ch>
2022-03-23 14:41:33 +0100

    disabled pool because of size for import to glitch

--- components/object-change.js
+++ components/object-change.js
@@ -5,18 +5,21 @@ AFRAME.registerComponent('change-object', {
     init: function(){
         var data = this.data;
         this.el.addEventListener('click', function(){
-            let o1 = document.querySelector('#pool');
+            //let o1 = document.querySelector('#pool');
             let o2 = document.querySelector('#motor');
             let o3 = document.querySelector('#gym-run-jog');
+            /*
             if (o1.getAttribute('visible') === true) {
                 o1.setAttribute('visible', 'false');
                 o2.setAttribute('visible', 'true');
-            } else if (o2.getAttribute('visible') === true){
+            } else
+            */
+            if (o2.getAttribute('visible') === true){
                 o2.setAttribute('visible', 'false');
                 o3.setAttribute('visible', 'true');
             } else {
                 o3.setAttribute('visible', 'false');
-                o1.setAttribute('visible', 'true');
+                o2.setAttribute('visible', 'true');
             }
         })
     }
